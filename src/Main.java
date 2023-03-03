b2265c7682147c8fd2d559eb67c05aa9c5a1b08b
Antonov333 <antonovsergei333@yahoo.com>
2023-03-03 13:35:10 +0300

    Homework 7 Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
+    static int hw = 7 ;
     public static void main(String[] args) {
-        System.out.println("\n Just started with homework 7 ))");
+        HWtitle();
+        task1();
+
+        HWbackCover() ;
+    }
+
+    public static void HWtitle() {
+        System.out.println("ДОМАШНЕЕ ЗАДАНИЕ №" + hw);
+        System.out.println(" ");
+    }
+
+    public static void task1 () {
+        System.out.println("Problem #1");
+        System.out.println("\nSolution to be provided soon");
+        System.out.println(" ");
+    }
+    public static void HWbackCover() {
+        System.out.println("Домашнее задание " + hw + " выполнено" );
+        System.out.println(" ");
     }
 }
+
+
